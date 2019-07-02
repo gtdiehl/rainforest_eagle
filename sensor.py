9b66769f3627fba95b66f2587937e75deb5218c0
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-07-02 12:01:13 -0700

    Fixed formatting issues

--- sensor.py
+++ sensor.py
@@ -3,11 +3,11 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (
     CONF_IP_ADDRESS, CONF_MONITORED_CONDITIONS, ENERGY_KILO_WATT_HOUR)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
 
 CONF_CLOUD_ID = 'cloud_id'
 CONF_INSTALL_CODE = 'install_code'
@@ -16,12 +16,19 @@ POWER_KILO_WATT = 'kW'
 _LOGGER = logging.getLogger(__name__)
 
 SENSORS = {
-    "instantanous_demand": ("Eagle-200 Meter Power Demand", POWER_KILO_WATT),
-    "summation_delivered": ("Eagle-200 Total Meter Energy Delivered", ENERGY_KILO_WATT_HOUR),
-    "summation_received": ("Eagle-200 Total Meter Energy Received", ENERGY_KILO_WATT_HOUR),
-    "summation_total": ("Eagle-200 Net Meter Energy (Delivered minus Received)", ENERGY_KILO_WATT_HOUR)
-	}
-	
+    "instantanous_demand": (
+        "Eagle-200 Meter Power Demand", POWER_KILO_WATT),
+    "summation_delivered": (
+        "Eagle-200 Total Meter Energy Delivered",
+        ENERGY_KILO_WATT_HOUR),
+    "summation_received": (
+        "Eagle-200 Total Meter Energy Received",
+        ENERGY_KILO_WATT_HOUR),
+    "summation_total": (
+        "Eagle-200 Net Meter Energy (Delivered minus Received)",
+        ENERGY_KILO_WATT_HOUR)
+        }
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP_ADDRESS): cv.string,
     vol.Required(CONF_CLOUD_ID): cv.string,
@@ -29,9 +36,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSORS)):
         vol.All(cv.ensure_list, [vol.In(list(SENSORS))])
 })
-	
+
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the Eagle-200 sensor."""
+    """Create the Eagle-200 sensor."""
     ip_address = config[CONF_IP_ADDRESS]
     cloud_id = config[CONF_CLOUD_ID]
     install_code = config[CONF_INSTALL_CODE]
@@ -40,10 +48,12 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         add_devices([Eagle(ip_address, cloud_id, install_code, condition,
                            SENSORS[condition][0], SENSORS[condition][1])])
 
+
 class Eagle(Entity):
     """Implementation of the Rainforest Eagle-200 sensor."""
 
-    def __init__(self, ip_address, cloud_id, install_code, sensor_type, name, unit):
+    def __init__(
+            self, ip_address, cloud_id, install_code, sensor_type, name, unit):
         """Initialize the sensor."""
         self._ip_address = ip_address
         self._cloud_id = cloud_id
@@ -69,7 +79,9 @@ class Eagle(Entity):
         return self._unit_of_measurement
 
     def update(self):
-        '''Get the energy demand from the Rainforest Eagle'''
+        """Get the energy demand from the Rainforest Eagle."""
         from eagle200_reader import EagleReader
-		
-        self._state = getattr(EagleReader(self._ip_address, self._cloud_id, self._install_code), self._type)()
+
+        self._state = getattr(EagleReader(
+            self._ip_address, self._cloud_id,
+            self._install_code), self._type)()
